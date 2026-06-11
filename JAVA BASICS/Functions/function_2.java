385e75cc8e4cd24b47a09706957014503cb49432
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-11 15:22:32 +0530

    Parameter Function

---
+++ JAVA BASICS/Functions/function_2.java
@@ -0,0 +1,10 @@
+//parameter function
+import java.util.Scanner;
+public class function_2{
+    static void sum(int x, int y){
+            System.out.print("Sum " + (x+y));
+        }
+        static void main(){
+        sum(10,50);
+        }
+    }
