b60d0f032d1f01d5f54287aa9aaec134dedb8ebb
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-11 15:14:01 +0530

    Add files via upload

---
+++ JAVA BASICS/Functions/functions_1.java
@@ -0,0 +1,18 @@
+//print 2 ka table by the method of function
+
+import java.util.Scanner;
+public class functions_1{
+    static void print2katable(){
+        for (int i = 1;i < 10; i++){
+            System.out.print(" " + 2*i);
+        }
+
+    }
+    static void main(){
+        print2katable();
+    }
+}
+
+
+
+
