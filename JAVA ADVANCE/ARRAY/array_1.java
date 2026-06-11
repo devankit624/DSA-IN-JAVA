27cb8c853dc5d8d9d645f4c4cffc4c8c5cac17c0
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-11 22:35:06 +0530

    Array Initialise and Declaration

---
+++ JAVA ADVANCE/ARRAY/array_1.java
@@ -0,0 +1,7 @@
+public class array_1{
+    public static void main(String[] args){
+        // array declaration and initialise 
+        int arr[] = {10,50,70,90};
+        System.out.println(arr[0]);
+    }
+}
