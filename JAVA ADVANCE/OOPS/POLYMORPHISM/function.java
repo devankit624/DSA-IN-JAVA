e00c9255042eb83f57b922e12ae2d0592a7c6273
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-17 22:04:23 +0530

    Contains Polymorphism All properties Also Static and Run Time Polymorphism

---
+++ JAVA ADVANCE/OOPS/POLYMORPHISM/function.java
@@ -0,0 +1,14 @@
+//static polymorphism
+// static polymorphism is the property of the oops in which contains one class contains
+// Many forms
+public class function{
+   public int add(int a , int b){
+    return a + b;
+   }
+   public int add(int a , int b , int c){
+    return a + b + c;
+   }
+   public double add(int a , int b , int c, int d){
+    return a +b +c +d ;
+   }
+}
---
+++ JAVA ADVANCE/OOPS/POLYMORPHISM/tasks.java
@@ -0,0 +1,12 @@
+// static polymorphism
+public class tasks {
+    public static void main(String[] args){
+        function f1 = new function();
+        System.out.println(f1.add(2,5));
+        System.out.println(f1.add(2,5 , 7));
+        System.out.println(f1.add(2,5 , 8 ,9));
+    }
+    
+}
+
+
