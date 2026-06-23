57cd3fd612a29880fcde8cc8f4cf3f8032f83b40
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-23 23:31:59 +0530

    Update and rename Main.java to prob-2.java

--- PROBLEMS SOLVED/prob-2.java
+++ PROBLEMS SOLVED/prob-2.java
@@ -1,6 +1,6 @@
 // Problems ->
 import java.util.Scanner;
-public class Main{
+public class prob-2{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // Pro - 2 ->
