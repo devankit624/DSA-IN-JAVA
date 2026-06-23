d22305de235176e3904b845195e1e84f31ac4386
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-23 23:31:28 +0530

    Power of 2 by Bitwise Operator

---
+++ PROBLEMS SOLVED/Main.java
@@ -0,0 +1,33 @@
+// Problems ->
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // Pro - 2 ->
+        // Check the number is the power of the 2 or Not
+        System.out.println("Enter Num ->");
+        int b = sc.nextInt();
+        int count = 0;
+        while(b != 0){
+            if ((b&1) != 0){
+                count++;
+            }
+            b = b >> 1;
+        }
+        System.out.println("The Set bit Count " + count);
+        if (count <= 1){
+            System.out.println("Power of 2");
+        }
+        else{
+            System.out.println("Not");
+        }
+        // Alter
+        if ((b & (b-1)) == 0 ){
+            System.out.println("power of 2");
+        }
+        else{
+            System.out.println("Not");
+        }
+        sc.close();
+    }
+}
