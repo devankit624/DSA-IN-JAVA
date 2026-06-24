44b0a4d2b3b01d1e38f366e9131656de6b84eaf1
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-24 22:02:23 +0530

    Shift Array by 1 position

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,20 @@
+// Shift element by 1 position
+public class Main{
+    static int[] shiftelement(int[] arr){
+        int size = arr.length;
+         int temp = arr[size-1];
+        for (int i = size - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+        return arr;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {4,5,6,8,9};
+        int[] shift = shiftelement(arr);
+        for(int num : shift){
+        System.out.print(num + ", ");
+        }
+    }
+}
