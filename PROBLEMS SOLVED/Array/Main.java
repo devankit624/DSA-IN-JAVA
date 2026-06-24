212f8caff99b6785f7a28ab5f0f96025f7fa10eb
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-24 15:16:16 +0530

    Zero and One Count

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    static int countzeroandone(int arr[]){
+        int size = arr.length;
+        int zerocount = 0;
+        int onecount = 0;
+        for(int i = 0; i < size; i++){
+            if (arr[i] > 0){
+                onecount++;
+            }
+            else{
+                zerocount++;
+            }
+        }
+        System.out.println("Zero count " + zerocount);
+        System.out.println("One count " + onecount);
+        return zerocount + onecount;
+    }
+
+    public static void main(String [] args){
+        int[] arr = {1,0,1,0,1,1,0,1};
+        int total = countzeroandone(arr);
+        System.out.println(total);
+    }
+}
