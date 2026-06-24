24294a9d50b33adfe8b92de66420de228ea11725
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-24 15:37:29 +0530

    Unsorted Element in Array

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,18 @@
+// Prob- Find the unsorted Element in the Array
+public class Main{
+        static int getunsorted(int arr[]){
+            for (int i = 0; i < arr.length ; i++){
+                if (arr[i+1] <= arr[i]){
+                    return arr[i+1];
+                }
+            }
+             return -1;
+        }
+       
+
+    public static void main(String[] args){
+        int arr[] = {1,2,5,4,9};
+        int find = getunsorted(arr);
+        System.out.println(find);
+}
+}
