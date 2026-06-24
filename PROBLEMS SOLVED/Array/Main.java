d64889bf2f801aed671d6c01b64a80c8961ed0d6
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-24 21:45:00 +0530

    Reverse of the Array

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,24 @@
+K// Kuch bhadiya Problems
+public class Main{
+    static int[] findreverse(int[] arr){
+        int size = arr.length;
+        int i = 0;
+        int j = size - 1;
+        int temp;
+        while(i<=j){
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
+}
+public static void main(String[] args){
+    int[] arr = {1,4,5,8,6};
+    int[] reverse = findreverse(arr);
+    for(int num : reverse){
+        System.out.print(num + ", ");
+    }
+}
+}
