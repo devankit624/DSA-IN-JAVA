a999d0214a19d00a3df66b33760ae27399d03db8
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-24 15:02:35 +0530

    multipy each number by 10

--- PROBLEMS SOLVED/Array/Main.java
+++ PROBLEMS SOLVED/Array/Main.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-    //Prob - 1
+    //Prob - 2
 
-    // finding the average of the array
-        System.out.println("Enter no of elements");
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
+
+        // mutiply each element by 10
+
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n ; i++){
+        for (int i = 0; i< n;i++){
             arr[i] = sc.nextInt();
-            sum = sum + arr[i];
         }
         // printing array
-        for (int i = 0; i <n ; i++){
+        for (int i =0; i <n; i++){
             System.out.print(arr[i] + " ");
         }
-        double average;
-        average = (double) sum / n;
-        System.out.println(average);
-        System.out.println(sum);
+        // multiply by 10
+        System.out.println("Enter Num which multiply");
+        int b = sc.nextInt();
+        int[] brr = new int[b];
+        for (int  i = 0; i <n ; i++){
+            brr[i] = b * arr[i];
+        }
+
+        for (int  i = 0; i <n ; i++){
+            System.out.print(brr[i] + " ");
+        }
+
+    }
+}
+
+
+        
 
 
         
