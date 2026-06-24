23d588beab0647838e12d795bfca99e617b19c2b
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-24 22:35:13 +0530

    Print Extreme Elements

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    static void printextreme(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            System.out.print(arr[i] + " ");
+            System.out.print(arr[j] + " ");
+            i++;
+            j--;
+        }
+        if (i == j) {
+            System.out.print(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 7, 8, 9};
+        printextreme(arr);
+    }
+}
