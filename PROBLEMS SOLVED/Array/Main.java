88236049b1823a43c8701ef1ca32836f2359771b
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-26 10:45:49 +0530

    Find dublicate in the Repeated Array list

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,31 @@
+// Find dublicate in the Repeated Array list
+import java.util.ArrayList;
+import java.util.List;
+public class Main{
+    static List<Integer> findmissing(int[] arr){
+    // marking
+    List<Integer> ans = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0 ; i< n ; i++){
+            int value = Math.abs(arr[i]);
+            int position = value - 1;
+            if (arr[position] > 0){
+                arr[position] = -arr[position];
+            }
+        }
+for (int j = 0 ; j < n ; j++){
+    if (arr[j] > 0){
+        int value = j+1;
+        ans.add(value);
+    }
+}
+return ans; 
+}
+public static void main(String[] args){
+    int[] arr = {1,2,2,3,5};
+    List<Integer> ans = findmissing(arr);
+    for (int num : ans){
+        System.out.print(num + ", ");
+ }
+}
+}
