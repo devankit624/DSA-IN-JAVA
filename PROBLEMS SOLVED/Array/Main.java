b97c91a75959d635b96db2d7bf3e26f39e8c3645
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-26 13:23:54 +0530

    Find first Dublicate Element

---
+++ PROBLEMS SOLVED/Array/Main.java
@@ -0,0 +1,24 @@
+// Problem ->
+// find first dublicate element
+import java.util.HashMap;
+public class Main{
+    static int finddublicate(int[] arr){
+        HashMap<Integer , Integer> freq = new HashMap<>();
+        for (int num : arr){
+            freq.put(num, freq.getOrDefault(num,0) + 1);
+        }
+        for (int i : arr){
+            if(freq.get(i) > 1){
+                return i;
+            } 
+        }
+        return -1;
+    }
+
+public static void main(String[] args){
+    int[] arr = {1,2,4,2,5,6};
+    int ans = finddublicate(arr);
+    System.out.println(ans);
+}
+}
+
