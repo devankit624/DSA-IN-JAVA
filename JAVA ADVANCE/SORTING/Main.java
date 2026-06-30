f3fa36cedff0baf9754e49f5f2c4668ae554459c
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-30 23:14:04 +0530

    Insertion sorting

---
+++ JAVA ADVANCE/SORTING/Main.java
@@ -0,0 +1,29 @@
+// Insertion sorting ->
+public class Main{
+    static int[] inssertionsort(int[] arr){
+        int n = arr.length;
+        for (int i =1; i < n ; i++){
+        int curr = i;
+        int prev = i-1;
+        int currvalue = arr[i];
+        while(prev>=0 && currvalue < arr[prev]){
+             arr[prev + 1] = arr[prev];
+             prev--;
+        }
+
+        // ek khali space ab bachi hogi
+        arr[prev + 1] = currvalue;
+        
+    }
+    return arr;
+}
+
+    public static void main(String[] args){
+        int[] arr = {1,4,7,5,2,3,6};
+        int[] ans = inssertionsort(arr);
+        for (int num : ans){
+            System.out.print(num);
+        }
+        System.out.println();
+    }
+}
