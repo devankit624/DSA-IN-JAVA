ca998189c345009451d9b77332218a86bc08ca82
Ankit Singh <ankitsingh163725@gmail.com>
2026-06-30 21:33:14 +0530

    Spiral Printing of the Matrix

---
+++ JAVA ADVANCE/ARRAY/2D ARRAY/Main.java
@@ -0,0 +1,44 @@
+import jav.util.List;
+import java.util.ArrayList;
+public class Main{
+    public Lst<Interger> spiralorder(int[][] matrix){
+        List<Integer> result = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int startingrow = 0;
+        int endinggrow = m - 1;
+        int startingcols = 0;
+        int endingcols = n- 1;
+
+        while(startingrow<=endinggrow && startingcols <= endingcols){
+            // startingcols and endingcols
+            for (int col = startingcols ; col < endingcols ; col++){
+                result.add(matrix[startingrow][col]);
+            }
+            startingrow++;
+            //starting row and the ending row
+            for (int row = startingrow ; row < endinggrow ; row+++){
+                result.add(matrix[row][endingcols]);
+            }
+            endingcols--;
+
+            // ending col and starting cols
+            for (int col = endingcols ; col >= startingcols ; col--){
+                result.add(matrix[endinggrow][col]);
+            }
+            endinggrow--;
+
+            // ending row and starting row
+            if (startingcols <= endingcols){
+            for (int row = endinggrow ; i>= startingrow ; i--){
+                result.add(matrix[row][startcol]);
+            }
+            startcol++;
+        }
+            
+        }
+        return result;
+
+    }
+
+}
