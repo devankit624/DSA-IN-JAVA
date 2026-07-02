a16cd90f874601b2358271aa5b4a43540e88a856
Ankit Singh <ankitsingh163725@gmail.com>
2026-07-02 23:27:22 +0530

    Delete JAVA ADVANCE/ARRAY directory

--- JAVA ADVANCE/ARRAY/array_1.java
+++
@@ -1,7 +0,0 @@
-public class array_1{
-    public static void main(String[] args){
-        // array declaration and initialise 
-        int arr[] = {10,50,70,90};
-        System.out.println(arr[0]);
-    }
-}
