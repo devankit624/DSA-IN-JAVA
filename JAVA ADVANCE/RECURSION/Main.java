a2287f840a9d9a16b149346fdfde28977d10c398
Ankit Singh <ankitsingh163725@gmail.com>
2026-07-14 23:22:12 +0530

    Find Max

---
+++ JAVA ADVANCE/RECURSION/Main.java
@@ -0,0 +1,17 @@
+
+// find max using recursion
+public class Main{
+    public static void linearsearch(int[] arr , int i ,  int target){
+        if(target == arr[i]){
+            System.out.print(arr[i]);
+        }
+        linearsearch(arr, i+1, target);
+    }
+    public static void main(String[] args){
+        int[] arr = {1,4,5,7,8,9,};
+        int i = 0;
+        int target = 9;
+        linearsearch(arr, i+1, target);
+    }
+}
+
