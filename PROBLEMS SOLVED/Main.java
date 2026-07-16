54ca61ddf20711feb8f7d8ed80fecf05ddcef354
Ankit Singh <ankitsingh163725@gmail.com>
2026-07-16 23:21:32 +0530

    Robber Steal Problem

---
+++ PROBLEMS SOLVED/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static int solve(int[]arr, int index){
+        if(index >= arr.length){
+            return 0;
+        }
+        // both include and exclude case
+        // include 
+        int includeans = arr[index] + solve(arr, index+2);
+        int excludeans = 0 + solve(arr, index+1);
+        int finalans = Math.max(includeans, excludeans);
+        return finalans;
+    }
+        // solving by the reccursion
+        public static int robberprob(int[] nums){
+            int index = 0;
+            int solve = solve(nums, index);
+            return solve;
+        }
+}
